a540bf0b986d024968e0f99f66ca13ca812b4e48
Dominik Przytuła <dominik.przytula@pollub.edu.pl>
2024-02-03 12:55:49 +0000

    condesandbox

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import OpenLayersMap from "vue3-openlayers";
+import OpenLayersMap from 'vue3-openlayers'
 
 import App from './App.vue'
 
 const app = createApp(App)
-app.use(OpenLayersMap);
+app.use(OpenLayersMap)
 app.mount('#app')
